5f80c080a133b46d5c1658c21e9ee29658891692
Michael Branconier <michaelbranconier@gmail.com>
2024-09-06 14:19:29 -0500

    Fix mobile padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const TWITTER_LINK = "https://x.com/mike_branc"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-16 pb-12">
+    <main className="flex min-h-screen flex-col items-center justify-between p-6 sm:p-16 pb-12">
       <CollegeCostCalculator />
       <p className="mt-4">Created by <a href={TWITTER_LINK} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline" style={{textDecoration: 'underline', color: 'blue'}}>Michael Branconier</a></p>
     </main>
